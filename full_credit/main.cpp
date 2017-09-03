02f2fc4e509667eab83fee83780966b47ccd53c1
Devi Prasad Tripathy <deviprasadtripathy@Devis-MacBook-Air.local>
2017-09-03 04:18:07 -0500

    called exam method

--- full_credit/main.cpp
+++ full_credit/main.cpp
@@ -2,6 +2,12 @@
 #include <"student.cpp">
 using namespace std;
 int main(){
-  Student student{}
+  Student student{"Bjarne Stroustrup"};
+  student.exam{100.0};
+  student.exam{90.0};
+  student.exam{80.0};
+  student.name();
+  student.average();
+  return 0;
 }
 
